9978ba0877288df108a918187b4f3336e3770553
张雅茹 <zhangyr@feicuiedu.com>
2016-11-01 09:54:49 +0800

    修改了文本框的文字

--- app/src/main/java/com/example/ideuse/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/ideuse/helloandroid/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.ideuse.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTvShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTvShow = (TextView) findViewById(R.id.tv_show);
+
+        mTvShow.setText("代码更新成功了!!");
     }
 }
